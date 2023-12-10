9fed8665c1e841b62fccb51f85fb16416fb6f80e
PrasanthKumar0 <142900866+PrasanthKumar0@users.noreply.github.com>
2023-12-10 16:41:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,6 @@ resource "aws_instance" "example" {
   ami                    = var.ami
   instance_type          = var.instance_type
   availability_zone      = var.availability_zone
-  iam_instance_profile   = var.iam_instance_profile
   key_name               = var.key_name
   security_groups        = var.security_groups
   subnet_id              = var.subnet_id
